5d5d8625e05cb8cdfacba43017edd67da0676485
kakariko3 <kakariko01@gmail.com>
2021-05-11 00:06:38 +0900

    Create: recipe_model

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '15bc930ec6836b82e5c9e8f6d5f432817a1b8c7b06eaf7e4b2aab73a00badafa3b73e760e33b4d5aef87c4bec415bc030d2f97b5450ab33015ffb4a93a789a76'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_08_175922) do
+ActiveRecord::Schema.define(version: 2021_05_10_070003) do
+
+  create_table "recipes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "body"
+    t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
